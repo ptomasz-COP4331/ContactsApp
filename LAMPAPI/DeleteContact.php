ab272ca1dfb38476aee275977cd48e0f22aea2b0
Ross Springstead <ross.springstead@knights.ucf.edy>
2023-02-10 15:25:43 -0500

    edit delete file

---
+++ LAMPAPI/DeleteContact.php
@@ -0,0 +1,40 @@
+<?php
+	$inData = getRequestInfo();
+
+	# $firstname = $inData["firstname"];
+	# $lastname = $inData["lastname"];
+	$ID = $inData["ID"];
+
+	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+	if ($conn->connect_error) 
+	{
+		returnWithError( $conn->connect_error );
+	} 
+	else
+	{
+		$stmt = $conn->prepare("Delete From Contacts from ID=?");
+		$stmt->bind_param("s", $ID);
+		$stmt->execute();
+		$stmt->close();
+		$conn->close();
+		returnWithError("");
+	}
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+
+?>
