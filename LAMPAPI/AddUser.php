382c3f61d351af4b338a6f21b3d251bf1478a1bb
Ross Springstead <ross.springstead@knights.ucf.edu>
2023-02-11 15:44:39 -0500

    fixing adduser

--- LAMPAPI/AddUser.php
+++ LAMPAPI/AddUser.php
@@ -13,28 +13,12 @@
 	} 
 	else
 	{
-		// Check to make sure username isn't taken first!
-		$stmt = $conn->prepare("SELECT * FROM Users WHERE Login=?");
-		$stmt->bind_param("s", $login);
-		$stmt->execute();
-		$res = $stmt->get_result();
-		$rows = mysqli_num_rows($res);
-
-		// If username taken, then return 409 conflict
-		if ($rows > 0) {
-			http_response_code(409);
-			return_with_error("409 State Conflict: Username Taken")
-		}
-
 		$stmt = $conn->prepare("INSERT into Users (FirstName,LastName,Login,Password) VALUES(?,?,?,?)");
 		$stmt->bind_param("ssss", $firstname, $lastname, $login, $password);
-		$new_user_id = $conn->insert_id;
 		$stmt->execute();
 		$stmt->close();
 		$conn->close();
-
-		http_response_code(200);
-		returnWithInfo($firstname, $lastname, $new_user_id);
+		returnWithError("");
 	}
 
 	function getRequestInfo()
@@ -54,9 +38,4 @@
 		sendResultInfoAsJson( $retValue );
 	}
 
-	function returnWithInfo( $firstName, $lastName, $id )
-	{
-		$retValue = '{"id":' . $id . ',"firstName":"' . $firstName . '","lastName":"' . $lastName . '","error":""}';
-		sendResultInfoAsJson( $retValue );
-	}
 ?>
