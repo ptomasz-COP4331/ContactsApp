af22dbfb4d322f7cdb4f0bc8b183ec35a48e2cfd
Ivy Zhang <ZIvy042003@gmail.com>
2023-02-12 14:49:17 -0500

    update

--- LAMPAPI/AddUser.php
+++ LAMPAPI/AddUser.php
@@ -14,27 +14,21 @@
 	else
 	{
 		try {
-			//PDO query execution goes here.
 			$stmt = $conn->prepare("INSERT into Users (FirstName,LastName,Login,Password) VALUES(?,?,?,?)");
 			$stmt->bind_param("ssss", $firstname, $lastname, $login, $password);
 			$stmt->execute();
 
-			if( mysql_errno() == 1062) {
+			$stmt->close();
+			$conn->close();
+
+			http_response_code(200);
+			returnWithError("User added!");		
+		} catch (PDOException $e) {
+			if ($e->getCode() == 23000) {
 				http_response_code(409);
 				returnWithError("Username Taken");
 			}
-			$stmt->close();
-			$conn->close();
-			returnWithError("Added User!");
 		}
-		catch (\PDOException $e) {
-				if ($e->errorInfo[1] == 1062) {
-					http_response_code(409);
-					returnWithError("Username Taken");
-						//The INSERT query failed due to a key constraint violation.
-				}
-		}
-
 	}
 
 	function getRequestInfo()
