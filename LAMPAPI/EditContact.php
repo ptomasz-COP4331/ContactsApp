ab15c15297adbb8ab44f2472ced630a766d998a7
Ivy Zhang <ZIvy042003@gmail.com>
2023-02-12 16:10:49 -0500

    update

--- LAMPAPI/EditContact.php
+++ LAMPAPI/EditContact.php
@@ -4,8 +4,7 @@
 	$name = $inData["name"];
 	$email = $inData["email"];
 	$phone = $inData["phone"];
-	//$date = $inData["date"];
-	$userid = $inData["userid"];
+	$cid = $inData["cid"];
 
 	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
 	if ($conn->connect_error) 
@@ -14,14 +13,10 @@
 	} 
 	else
 	{
-		$stmt = $conn->prepare("UPDATE Contacts SET 'Name'=$name 'Phone'=$phone 'Email'=$email WHERE UserID=$userid");
+		$stmt = $conn->prepare("UPDATE Contacts SET Name = ?, Email = ?, Phone = ? WHERE ID = ?);
+		$stmt->bind_param("ssss", $name, $email, $phone, $cid);
 		$stmt->execute();
-
-		$result = $stmt->get_result();
-
-		$row = $result->fetch_assoc();
-		returnWithInfo( $row['Name'], $row['Email'], $row['Phone'], $row['DateCreated'] );
-
+		
 		$stmt->close();
 		$conn->close();
 		returnWithError("");
--- js/code.js
+++ js/code.js
@@ -221,6 +221,14 @@ function editContact () {
   }
 }
 
+function editRow (i) {
+  // document.getElementById('save')
+}
+
+function saveRow (i) {
+
+}
+
 function deleteRow (i) {
   const name = document.getElementById('cna-' + i).innerText
   const con = confirm(`Would you like to delete ${name} from your contacts?`)
@@ -290,7 +298,7 @@ function loadContacts () {
           lContacts += "<td id='cem-" + i + "'>" + '<span>' + jsonObject.results[i].phone + '</span></td>'
 
           // TODO: Add buttons into last column for edit/delete
-          lContacts += `<td><button onclick="editRow(${i})"><span class="material-symbols-rounded">edit</span><button onclick="saveRow(${i})"><span class="material-symbols-rounded">save</span></button><button onclick="deleteRow(${i})"><span class="material-symbols-rounded">delete</span></button></td>`
+          lContacts += `<td><button id='eb-${i}' onclick="editRow(${i})"><span class="material-symbols-rounded">edit</span><button id='sb${i}' onclick="saveRow(${i})"><span class="material-symbols-rounded">save</span></button><button id='db${i}' onclick="deleteRow(${i})"><span class="material-symbols-rounded">delete</span></button></td>`
           lContacts += '</tr>'
         }
 
